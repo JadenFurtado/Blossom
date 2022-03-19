f0e7637dd5e83d21507b776bc0ad76318555eec1
JadenFurtado <52862591+JadenFurtado@users.noreply.github.com>
2022-03-19 09:44:22 +0530

    changes paths

--- js/database.js
+++ js/database.js
@@ -1,6 +1,6 @@
 // Get a reference to the database service
 var database = firebase.database();
-var redirectURL = "/login.html";
+var redirectURL = "selfHelpApp/login.html";
 
 
 //function to add a task to the user
