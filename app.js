82aaae55ba17ce82e222a1bb07456486f39c836a
JadenFurtado <52862591+JadenFurtado@users.noreply.github.com>
2022-05-22 12:46:41 +0530

    changing the location of sw

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('https://jadenfurtado.github.io/Blossom/js/sw.js')
+    navigator.serviceWorker.register('https://jadenfurtado.github.io/Blossom/sw.js')
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log('service worker not registered', err));
   }
